159a501c03822aa67a83412f92643c29abdac4ee
Ihor Dziabenko <igordzyabenko32@gmail.com>
2023-05-12 18:07:29 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,135 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="controls">
+      <input v-model="title" placeholder="Issue title">
+      <button @click="addIssue" class="btn add">Add issue</button>
+    </div>
+    <div class="issue-list" v-if="issues.length">
+      <div class="issue" v-for="item in issues" :key="item.id">
+        <h4 class="issue__header"><span>id:{{ item.id }}</span> {{ item.subject }}</h4>
+        <button @click="removeIssue(item.id)" class="btn remove">Remove</button>
+      </div>
+    </div>
+    <div class="issue-list" v-else>
+      Good job, no more issues
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
-export default {
+export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      issues: [],
+      loaded: false,
+      title: ''
+    }
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      fetch('/issues.json')
+          .then(response => response.json())
+          .then(data => {
+            this.issues = data.issues;
+            this.loaded = true;
+          })
+    },
+    addIssue() {
+      const issue = {
+        issue:
+            {
+              project_id: 1,
+              subject: this.title ? this.title : 'Untitled',
+              priority_id: 4
+            }
+      };
+      fetch('/issues.json', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Redmine-API-Key": process.env.VUE_APP_API_KEY
+        },
+        body: JSON.stringify(issue),
+      }).then(async resp => {
+        const issueCreated = await resp.json().then(data => data.issue);
+        this.title = "";
+        if (resp.status === 201) { // create success
+          this.issues = [...this.issues, {...issueCreated}];
+        }
+      }).catch(e => {
+        console.warn(e)
+      });
+    },
+    removeIssue(id) {
+      fetch(`/issues/${id}.json`, {
+        method: "DELETE",
+        mode: "cors",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Redmine-API-Key": process.env.VUE_APP_API_KEY
+        },
+      }).then(resp => {
+        if (resp.status === 204) { // remove success
+          this.issues = this.issues.filter(item => item.id != id);
+        }
+      }).catch(e => {
+        console.warn(e)
+      })
+    }
   }
-}
+})
 </script>
 
 <style>
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.wrapper {
+  padding: 40px;
+}
+
+.controls {
+  display: flex;
+}
+
+.issue-list {
+  margin-top: 30px;
+}
+
+.issue {
+  border: 1px solid lightgray;
+  padding: 10px;
+  border-radius: 4px;
+}
+
+.issue + .issue {
+  margin-top: 20px;
+}
+
+.issue__header {
+  margin-top: 0;
+  margin-bottom: 5px;
+  line-height: 1.35;
+}
+
+.remove {
+  display: block;
 }
 </style>
